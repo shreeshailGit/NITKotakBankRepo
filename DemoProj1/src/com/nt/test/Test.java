5e635874504267d412ad88b7415c71687ff1417d
Dev1 <Dev1@gmail.com>
2020-04-11 00:15:52 +0530

    Test class development is done

---
+++ DemoProj1/src/com/nt/test/Test.java
@@ -0,0 +1,14 @@
+package com.nt.test;
+
+import com.nt.beans.Utility;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Utility u=new Utility();
+		
+    System.out.println(u.getSysdate());
+   System.out.println("Test.main()");
+	}
+
+}
